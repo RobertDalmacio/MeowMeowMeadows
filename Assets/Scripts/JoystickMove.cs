1155fd1f477159edd7d56bbd33e864d9d4cc3e94
RobertDalmacio <99753940+RobertDalmacio@users.noreply.github.com>
2025-02-12 21:29:20 -0500

    Initial commit

---
+++ Assets/Scripts/JoystickMove.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class JoystickMove : MonoBehaviour
+{
+    public float playerSpeed;
+    
+    public Joystick movementJoystick;
+    public Animator animator;
+    
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // player moving joystick
+        if (movementJoystick.Direction.magnitude > 0)
+        {
+            Vector2 direction = new Vector2(movementJoystick.Direction.x * playerSpeed, movementJoystick.Direction.y * playerSpeed);
+            rb.linearVelocity = direction;
+            // let animator know that player is moving with current direction
+            AnimateMovement(direction);
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+            // let animator know that player is no longer moving
+            AnimateMovement(Vector2.zero);
+        }
+    }
+
+    // Updates player animator controller
+    void AnimateMovement(Vector2 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                // transistions animation to running state
+                animator.SetBool("isMoving", true);
+                // updates animation based on direction
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                // transistions animation to idle state
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
