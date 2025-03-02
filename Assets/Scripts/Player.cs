906df01ec94d986ce040e1d5a7dfc1c102db3fc2
JacklynBecker <200626295@student.georgianc.on.ca>
2025-03-02 12:59:21 -0500

    added plowed ground and action button to plow the ground

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,30 @@
+using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Inventory inventory;
 
+    public Button actionButton;
+
     private void Awake()
     {
         inventory = new Inventory(21);
+        actionButton.onClick.AddListener(InteractWithGround);
+    }
+
+    public void InteractWithGround()
+    {
+
+        Vector3Int position = new Vector3Int((int)transform.position.x, (int)transform.position.y,0);
+
+        if(GameManager.instance.tileManager.IsInteractable(position))
+        {
+            Debug.Log("Tile is interactable!");
+            GameManager.instance.tileManager.SetInteracted(position);
+        }
+
     }
 }
